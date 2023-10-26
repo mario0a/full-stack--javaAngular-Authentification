b211bcac235652547797ff6d8bd53381ac266749
mario0a <mariusmvondo@gmail.com>
2023-10-26 14:42:36 +0200

    initialisation du projet partie backend

---
+++ back_Springboot/backend/src/main/java/com/messagerie/jwt/backend/BackendAppliMessagerieJwtApplication.java
@@ -0,0 +1,13 @@
+package com.messagerie.jwt.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppliMessagerieJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAppliMessagerieJwtApplication.class, args);
+	}
+
+}
---
+++ back_Springboot/backend/src/test/java/com/messagerie/jwt/backend/BackendAppliMessagerieJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messagerie.jwt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAppliMessagerieJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
