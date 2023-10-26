fa31bc210c1ca997afd975a55734acfa660d2999
mario0a <mariusmvondo@gmail.com>
2023-10-26 16:36:16 +0200

    installation de axios et bootstrap. creation du service axios configuration de la communication avec le backend dans le fichier axios.service.ts
    creation d'un nouveau component pour afficher le contenu du backend.
    
    //creation d'une variable dans laquelle on stockera la reponse du backend.
    
    ajout de la ligne "node_modules/bootstrap/dist/css/bootstrap.min.css"
    dans le angular.json a coté de style.

--- front_Angular/frontendLoginJWT/src/app/app.module.ts
+++ front_Angular/frontendLoginJWT/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
