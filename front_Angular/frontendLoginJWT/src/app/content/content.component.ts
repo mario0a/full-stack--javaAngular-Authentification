cef399ea2516d7cb3827aea9b0e9f7c37535a05c
mario0a <mariusmvondo@gmail.com>
2023-10-28 01:01:08 +0200

    ajout du component pour le formulaire de connexion creation du component de bienvenue.
    ajout du formulaire de connexion ok et methode ok pour le front

--- front_Angular/frontendLoginJWT/src/app/content/content.component.ts
+++ front_Angular/frontendLoginJWT/src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AxiosService} from "../axios.service";
 
 @Component({
   selector: 'app-content',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+// on ajoute le Axios service et on l'injecte dans le constructeur
 
+  constructor(private axiosService: AxiosService) {
+  }
+
+  onLogin(input: any) { // mothode appelé et utilisé dans le contentcomponent
+    //on demande le endpoint de connexion
+    this.axiosService.request(
+      "POST",
+      "/login",
+      {
+        login: input.login,
+        password: input.password
+      }
+    );
+  }
 }
