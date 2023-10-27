76ae4abecefb83b2005b7412a71776d98b830b73
mario0a <mariusmvondo@gmail.com>
2023-10-28 00:40:02 +0200

    ajout du component pour le formulaire de connexion creation du component de bienvenue.
    ajout du formulaire de connexion ok

--- front_Angular/frontendLoginJWT/src/app/app.module.ts
+++ front_Angular/frontendLoginJWT/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +8,7 @@ import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth.component';
 import { BienvenueContentComponent } from './bienvenue-content/bienvenue-content.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +17,14 @@ import { LoginFormComponent } from './login-form/login-form.component';
     HeaderComponent,
     AuthComponent,
     BienvenueContentComponent,
-    LoginFormComponent
+    LoginFormComponent,
+    ContentComponent,
+
   ],
   imports: [
     BrowserModule,
+    // pour afficher un formulaire, on doit ajouter un nouveau module dans app.module.ts
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front_Angular/frontendLoginJWT/src/app/login-form/login-form.component.ts
+++ front_Angular/frontendLoginJWT/src/app/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent {
+
+  //creation du formulaire de connexion
+// on cree un champ de sortie
+  // de cette facon, la methode submit sera dans le composant content, dans le composant parent.
+  //Ainsi, avoir la demande de connexion dans le composant parent me permet de gerer la réponse et de passer
+  // du formulaire de connexion au composant de contenu d'authentification une fois authentifié
+  @Output() onSubmitLoginEvent = new EventEmitter();
+
+  // on cree les variables qui contiendront le login et le mot de passe.
+  login: string ="";
+  password: string = "";
+
+  // cette methode permettra d'emettre la variable de sortie
+  onSubmitLogin(): void {
+    this.onSubmitLoginEvent.emit({"login": this.login,"password": this.password}); // pour afficher un formulaire, on doit ajouter un nouveau module dans app.module.ts
+  }
 
 }
