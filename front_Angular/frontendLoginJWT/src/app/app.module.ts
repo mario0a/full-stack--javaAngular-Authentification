91da8988cf354ea23663d75a60d6dc4be462fd03
mario0a <mariusmvondo@gmail.com>
2023-10-27 23:35:50 +0200

    ajout du component pour le formulaire de connexion creation du component de bienvenue

--- front_Angular/frontendLoginJWT/src/app/app.module.ts
+++ front_Angular/frontendLoginJWT/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { AuthComponent } from './auth/auth.component';
+import { BienvenueContentComponent } from './bienvenue-content/bienvenue-content.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    AuthComponent
+    AuthComponent,
+    BienvenueContentComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
